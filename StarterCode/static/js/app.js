48d42025e82ae69981712e3dd0a06a7b039572f3
yeagernolan19 <127434000+yeagernolan19@users.noreply.github.com>
2023-07-09 20:54:35 -0400

    first commit

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,5 @@
+let url='https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.';
+
+d3.json(url).then(function(data) {
+ console.log(data); 
+});
